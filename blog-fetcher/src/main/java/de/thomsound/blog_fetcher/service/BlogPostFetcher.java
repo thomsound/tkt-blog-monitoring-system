c5c03f82967a0378e0b2f19b9bb074a7c8ab21e7
thomsound <thomsound@users.noreply.github.com>
2025-02-03 20:43:02 +0100

    only fetch recently modified posts

--- blog-fetcher/src/main/java/de/thomsound/blog_fetcher/service/BlogPostFetcher.java
+++ blog-fetcher/src/main/java/de/thomsound/blog_fetcher/service/BlogPostFetcher.java
@@ -7,27 +7,46 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.TimeZone;
 
 
 @Service
 public class BlogPostFetcher {
     private static final Logger log = LoggerFactory.getLogger(BlogPostFetcher.class);
+
     private final WebClient webClient;
+
+    private Date lastPoll = null;
 
     public BlogPostFetcher(WebClient webClient) {
         this.webClient = webClient;
     }
 
     public Flux<Post> fetchPosts() {
-        return fetchPages(1);
+        Optional<String> after = this.lastPoll == null ? Optional.empty() : Optional.of(getISODateString(this.lastPoll));
+        this.lastPoll = new Date();
+
+        return fetchPages(1, after, List.of("id", "modified_after", "title", "content"));
     }
 
-    private Flux<Post> fetchPages(int page) {
+    private String getISODateString(Date date) {
+        SimpleDateFormat sdf;
+        sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return sdf.format(date);
+    }
+
+    private Flux<Post> fetchPages(int page, Optional<String> after, List<String> fields) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/wp-json/wp/v2/posts")
                         .queryParam("page", page)
+                        .queryParam("_fields", String.join(",",fields))
+                        .queryParamIfPresent("modified_after", after)
                         .build())
 
                 .exchangeToFlux(clientResponse -> {
@@ -40,7 +59,7 @@ public class BlogPostFetcher {
                         return currentPagePosts;
                     }
 
-                    return currentPagePosts.concatWith(fetchPages(page + 1));
+                    return currentPagePosts.concatWith(fetchPages(page + 1, after, fields));
                 });
     }
 }
